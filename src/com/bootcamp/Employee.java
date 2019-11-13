a97867947c3fc619a4dac144327cc86c0b7e45b0
snoob <supernoobice@gmail.com>
2019-11-13 20:25:01 +0800

    First init: Employee class

---
+++ src/com/bootcamp/Employee.java
@@ -0,0 +1,39 @@
+package com.bootcamp;
+
+public abstract class Employee {
+    public String name;
+    public double baseSalary;
+    public static int ID;
+    public Employee manager;
+
+    public Employee(String name, double baseSalary) {
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
+
+    public double getBaseSalary() {
+        return this.baseSalary;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getEmployeeID() {
+        return ID;
+    }
+
+    public Employee getManager() {
+        return this.manager;
+    }
+
+    public boolean equals(Employee other) {
+        return this.getEmployeeID() == other.getEmployeeID();
+    }
+
+    public String toString() {
+        return this.getEmployeeID() + " " + this.getName();
+    }
+
+    public abstract String employeeStatus();
+}
